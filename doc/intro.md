c816fcdc73da8395b3a83cdb0cfb25916e5879ef
Logan Campbell <colin@logaan.net>
2019-05-12 22:33:42 +1000

    lein new zen-search-clj

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to zen-search-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/zen_search_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns zen-search-clj.core-test
+  (:require [clojure.test :refer :all]
+            [zen-search-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
