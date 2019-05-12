311163174b2663128559c8964f4c24b7b0056440
Logan Campbell <colin@logaan.net>
2019-05-13 00:10:18 +1000

    happy path hard coded data

--- src/zen_search_clj/core.clj
+++ src/zen_search_clj/core.clj
@@ -1,29 +1,38 @@
 (ns zen-search-clj.core)
 
-;; States:
-;;
-;; want-dataset -> want-field -> want-query -> want-dataset
+(def data
+  {"users" {"_id" {"1" "Logan"
+                   ""  "Pei Shi"}}})
 
-(def greeting
-  "Hello! Type exit to exit.")
+(def prompts
+  {:file  "Which file would you like to search?"
+   :field "Which field should we look in?"
+   :value "What's the value you'd like?"})
 
-(def exit
-  "Goodbye!")
+(def initial-state
+  {:step  :file
+   :file  nil
+   :field nil})
 
-(defn tick [state input]
-  {:state state
-   :output (str "You typed: " input)})
+(defmulti tick (fn [state _input] (:step state)))
 
-(defn main []
-  (println greeting)
+(defmethod tick :file [state file]
+  {:state (assoc state :step :field :file file)})
 
-  (loop [state {}]
+(defmethod tick :field [state field]
+  {:state (assoc state :step :value :field field)})
+
+(defmethod tick :value [{:keys [file field]} value]
+  {:state initial-state
+   :output (or (get-in data [file field value])
+               "No result found.")})
+
+(defn -main [& _args]
+  (loop [state initial-state]
+    (println (prompts (:step state)))
+
     (let [input (read-line)]
       (when-not (= input "exit")
         (let [{:keys [state output]} (tick state input)]
-          (println output)
-          (recur {})))))
-
-  (println exit))
-
-(main)
+          (if output (println output))
+          (recur state))))))
