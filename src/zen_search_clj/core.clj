014d2637625dd69037c5dc7590421c91ab314688
Logan Campbell <colin@logaan.net>
2019-05-29 23:15:56 +1000

    relationships and yaml output

--- src/zen_search_clj/core.clj
+++ src/zen_search_clj/core.clj
@@ -1,7 +1,21 @@
 (ns zen-search-clj.core
   (:require [cheshire.core :refer [parse-stream]]
-            [clojure.java.io :refer [resource reader]]
-            [clojure.pprint :refer [pprint]]))
+            [yaml.core :refer [generate-string]]
+            [clojure.java.io :refer [resource reader]]))
+
+(def files
+  ["users" "tickets" "organizations"])
+
+(def relationships
+  [["tickets" "submitter" "users"]
+   ["tickets" "assignee"  "users"]
+   ["tickets" "organization" "organizations"]
+   ["users"   "organization" "organizations"]])
+
+(def prompts
+  {:file  "Which file would you like to search?"
+   :field "Which field should we look in?"
+   :value "What's the value you'd like?"})
 
 (defn load-json [file]
   (for [entity         (-> (str file ".json") resource reader parse-stream)
@@ -11,12 +25,21 @@
 
 (def data
   (reduce (partial merge-with into)
-          (flatten (map load-json ["users" "tickets" "organizations"]))))
+          (flatten (map load-json files))))
 
-(def prompts
-  {:file  "Which file would you like to search?"
-   :field "Which field should we look in?"
-   :value "What's the value you'd like?"})
+(defn add-related-data [file entity]
+  (reduce (fn [{:strs [:_id] :as entity} [many field one]]
+            (let [field-id (str field "_id")]
+              (cond (= file many)
+                    (assoc entity field
+                           (data [one "_id" (str (entity field-id))]))
+
+                    (= file one)
+                    (assoc entity (str many "-as-" field)
+                           (data [many field-id (str (entity "_id"))]))
+
+                    :else entity)))
+          entity relationships))
 
 (defmulti tick (fn [state _input] (:step state)))
 
@@ -28,8 +51,9 @@
 
 (defmethod tick :value [{:keys [file field]} value]
   {:state  {:step :file}
-   :output (if-let [result (data [file field value])]
-             (with-out-str (pprint result))
+   :output (if-let [result (->> (data [file field value])
+                                (map (partial add-related-data file)))]
+             (generate-string result)
              "No result found.")})
 
 (defn -main [& _args]
@@ -41,3 +65,5 @@
         (let [{:keys [state output]} (tick state input)]
           (if output (println output))
           (recur state))))))
+
+(-main)
